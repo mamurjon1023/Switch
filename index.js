507546277de1aaa10db5ccf193d6638b01fa3c7d
mamurjon1148 <mamurjonovmamurjon01@gmail.com>
2024-06-20 17:01:39 +0500

    done

---
+++ index.js
@@ -0,0 +1,68 @@
+let son = prompt("Sonni kiriting")
+
+switch (son) {
+    case "1":
+        alert("dushanba");
+        break;
+    case "2":
+        alert("seshanba");
+        break;
+    case "3":
+        alert("chorshanba");
+        break;
+    case "4":
+        alert("payshanba");
+        break;
+    case "5":
+        alert("juma");
+        break;
+    case "6":
+        alert("shanba");
+        break;
+    case "7":
+        alert("yakshanba");
+        break;
+     default:"Bunday hafta kuni yo'q"
+}
+
+// let day = prompt("Hafta kunni kiriting");
+
+// switch (day) {
+//     case "monday":
+//         alert(`${day} -> dushanba`);
+//         break;
+//     case "tuesday":
+//         alert(`${day} -> seshanba`);
+//         break;
+//     case "wendesday":
+//         alert(`${day} -> chorshanba`);
+//         break;
+//     case "thursday":
+//          alert(`${day} -> payshanba`);
+//          break;
+//     case "friday":
+//         alert(`${day} -> juma`);
+//         break;
+//     case "saturday":
+//         alert(`${day} -> shanba`);
+//         break;
+//     case "sunday":
+//         alert(`${day} -> yakshanba`);
+//         break;
+// }
+
+// if (day === "Monday") {
+//     alert(`${day} - dushanba`)
+// } else if (day === "tuesday") {
+//     alert(`${day} - seshanba`)
+// } else if (day === "wendesday") {
+//     alert(`${day} - chorshanba`)
+// } else if (day === "thursday") {
+//     alert(`${day} - payshanba`)
+// } else if (day === "friday") {
+//     alert(`${day} - juma`)
+// } else if (day === "saturday") {
+//     alert(`${day} - shanba`)
+// } else if (day === "sunday") {
+//     alert(`${day} - yakshanba`)
+// }  
